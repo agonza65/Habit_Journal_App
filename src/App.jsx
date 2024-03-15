882fc38af4dc670b7dbec4fcf6e2e232f8638f69
agonza65 <62278955+agonza65@users.noreply.github.com>
2024-03-15 16:26:19 -0400

    syntax error

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { MainPage } from './components/mainPage/mainPage'
-import { JournalPage } from './components/newJournalPage/newJournalPage'
 import { NewJournalPage } from './components/newJournalPage/newJournalPage'
 import { GoalPage } from './components/goalPage/goalPage'
 import { NewGoalPage } from './components/newGoalPage/newGoalPage'
 import { ProgressPage } from './components/progressPage/progressPage'
+import { JournalPage } from './components/journalPage/journalPage'
 
 function App() {
   const [count, setCount] = useState(0)
