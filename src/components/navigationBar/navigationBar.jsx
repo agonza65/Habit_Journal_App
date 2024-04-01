f175927b83fb6cb7272cc1ddd060657723bfa1c6
agonza65 <62278955+agonza65@users.noreply.github.com>
2024-03-31 20:14:08 -0400

    03-31-24 Updates

--- src/components/navigationBar/navigationBar.jsx
+++ src/components/navigationBar/navigationBar.jsx
@@ -1,6 +1,18 @@
+import { Button } from "../button/button"
+
 //Navigation Bar component
 export const NavigationBar = () => {
+	const [currentPage, setPage] = useState("Main Page");
+
+	function handleSelect(selectedButton) {
+        console.log(selectedButton);
+    }
 	return (
-		<div> Navigation Bar </div>
+		<div> 
+			<h2> Navigation Bar </h2>
+			<Button onSelect = {() => handleSelect("Main Page")}> Main Page </Button>
+			<Button onSelect = {() => handleSelect('Journals or Goals')}> Journals or Goals </Button>
+			<Button onSelect = {() => handleSelect('Progress')}> Progress </Button>
+		</div>
 	);
 }
