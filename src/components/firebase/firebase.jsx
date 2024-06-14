e8b5513dd74c422b8f6950170e1206df1131329b
agonza65 <62278955+agonza65@users.noreply.github.com>
2024-06-14 02:46:16 -0400

    updating firebase file

--- src/components/firebase/firebase.jsx
+++ src/components/firebase/firebase.jsx
@@ -1,21 +1,20 @@
-import "firebase/firestore";
-// Fire base configuration
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAKzvm7vbuTvJB_TOHhqfgvdV7l2dsW-94",
-    authDomain: "habit-journal-app.firebaseapp.com",
-    databaseURL: "https://habit-journal-app-default-rtdb.firebaseio.com",
-    projectId: "habit-journal-app",
-    storageBucket: "habit-journal-app.appspot.com",
-    messagingSenderId: "361762564020",
-    appId: "1:361762564020:web:080dbe134f3d5b8fe974e1",
-    measurementId: "G-G7TYGRRV7L"
-  };
+  apiKey: "AIzaSyAKzvm7vbuTvJB_TOHhqfgvdV7l2dsW-94",
+  authDomain: "habit-journal-app.firebaseapp.com",
+  databaseURL: "https://habit-journal-app-default-rtdb.firebaseio.com",
+  projectId: "habit-journal-app",
+  storageBucket: "habit-journal-app.appspot.com",
+  messagingSenderId: "361762564020",
+  appId: "1:361762564020:web:080dbe134f3d5b8fe974e1",
+  measurementId: "G-G7TYGRRV7L"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-// Initialize firebase
-const firebaseApp = firebase.intitalizeApp(firebaseConfig);
 
-// Initialize cloud firestore 
-const db = firebaseApp.firestore();
-const auth = firebase.auth();
-
-export default { db, auth };
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
